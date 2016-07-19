7539a7d8b627ab3f9d3d78d9cb6b97496dcc5249
Nicolas Mengin <nicolas.mengin@lyra-network.com>
2016-07-19 17:35:11 +0200

    FIX travis configuration.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 mvn clean deploy --settings .travis/settings.xml -Prelease-profile,generate-ws-stubs -DskipTests=true -B -U
+ret=$?
+
+echo "************"$ret"************"
+
+exit $ret
