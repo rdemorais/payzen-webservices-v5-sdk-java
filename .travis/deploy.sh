de6e7b3df7b67822b9dfef9809a8807011b3e123
Nicolas Mengin <nicolas.mengin@lyra-network.com>
2016-07-19 11:19:06 +0200

    MODIFY MAVEN profiles management to simplify travis builds.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mvn clean deploy --settings .travis/settings.xml -Prelease-profile -DskipTests=true -Dgenerate.ws.stubs=true  -B -U
+mvn clean deploy --settings .travis/settings.xml -Prelease-profile,generate-ws-stubs -DskipTests=true -B -U
