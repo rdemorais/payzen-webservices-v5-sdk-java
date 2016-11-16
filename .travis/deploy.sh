978bc465a931f1a2a7cfdd78e551edb261b36427
Nicolas Mengin <nicolas.mengin@lyra-network.com>
2016-11-16 11:10:30 +0100

    MODIFU before_install step in the way to try using shell script to rempalce all the command lines

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-gpg2 --list-keys
-gpg2 --keyserver hkp://pool.sks-keyservers.net --send-keys a979a5f6682d6268
 mvn clean deploy --settings .travis/settings.xml -Prelease-profile -DskipTests=true -B -U
