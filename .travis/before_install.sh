6d7c5150185629c0be4ead189927b1278eed4022
Nicolas Mengin <nicolas.mengin@lyra-network.com>
2016-11-16 11:14:28 +0100

    MODIFY travis before_install step, back to the commands directly in the file...

--- .travis/before_install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    echo $GPG_SECRET_KEYS | base64 --decode | $GPG_EXECUTABLE --import
-    echo $GPG_OWNERTRUST | base64 --decode | $GPG_EXECUTABLE --import-ownertrust
-    $GPG_EXECUTABLE  --list-keys
-    $GPG_EXECUTABLE  --keyserver hkp://pool.sks-keyservers.net --send-keys a979a5f6682d6268
-fi
